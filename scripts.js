27e29643c413650b7744827f83378ba2cb7fc495
MZMahid <78899301+MZMahid@users.noreply.github.com>
2026-01-02 22:38:57 +0600

    implement multi axis rotation at the same time

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 /** @type {HTMLCanvasElement} */
 
+const body = document.querySelector("body");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let h = canvas.height;
 let w = canvas.width;
+let ct = 0;
+
+// let ZnegBtn = document.querySelector("#Zneg");
+// let XposBtn = document.querySelector("#Xpos");
+// body.appendChild(ZnegBtn);
 
 function clear() {
     ctx.fillStyle = "#050223ff";
@@ -16,7 +22,7 @@ function point({x, y}) {
     ctx.fillRect(x - s/2 , y - s/2, s, s);
     return {x: x - s/2,
             y: y - s/2,
-            };
+    };
 }
 
 function cordTranslate({x, y}) {
@@ -25,9 +31,9 @@ function cordTranslate({x, y}) {
     return {x, y};
 }
 
-function translate({x, y, z}) {
-    z += dz;
-    return {x, y, z};
+function translate(p,dx=0, dy=0, dz=0) {
+    p.z -= dz;
+    return p;
 }
 
 function project({x, y, z}) {
@@ -37,17 +43,51 @@ function project({x, y, z}) {
 }
 
 function rotate({x, y, z}){
-    let cos = Math.cos(angle);
-    let sin = Math.sin(angle);
-    // x = x * Math.cos(angle) - z * Math.sin(angle);
-    // z = x * Math.sin(angle) + y * Math.cos(angle);
-    return {
-        x: x * cos - z * sin,
-        y: y,
-        z : x * sin + z * cos - 1, //step back 1 unit to view it from outside.
+    p = {x, y, z};
+
+    function rotX({x, y, z} ,angle) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        return {
+            x: x,
+            y: y*cos - z*sin,
+            z: y*sin + z*cos,
+        }
     }
 
+    function rotY({x, y, z}, angle) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        return {
+            x: x*cos + z*sin,
+            y: y,
+            z: -x*sin + z*cos
+        }
+    }
+
+    function rotZ({x, y, z}, angle) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        return {
+            x: x*cos - y*sin,
+            y: x*sin + y*cos,
+            z: z
+        }
+    }
+    // let options = [rotX, rotY, rotZ];
+    // let pick = Math.round(Math.random() * 2);
+    // options[pick](angle);
+    let p1 = rotX(p, angle);
+    let p2 = rotY(p1, angle);
+    let p3 = rotY(p2, angle);
+    p1.z -= 1;
+    p2.z -= 1;
+    p3.z -= 1;
+    return p3;
+
+
 }
+    // let pF = {x: (p1.x * p2.x), y: (p1.y * p2.y), z: (p1.z * p2.z)};
 
 function drawLine(p1, p2) {
     ctx.strokeStyle = "lime";
@@ -62,25 +102,25 @@ function drawLine(p1, p2) {
 
 function animate() {
     clear();
+    let p1, p2;
     for (const e of edges) {
         let v1 = vertices[ e[0] ];
         let v2 = vertices[ e[1] ];
-        let p1 = cordTranslate( project( rotate(v1) ) );
+        p1 = cordTranslate( project( rotate(v1) ) );
         p2 = cordTranslate( project( rotate(v2) ) );
-        // let p2 = {x:69, y:69};
         // console.log(p1, p2);
         drawLine(p1,p2);
     }
-    dz += 1*dt;
-    angle += Math.PI / 3 * dt;
-    // drawLine(vertices);
+    // for(const p1 of vertices) {
+    //     point(cordTranslate( project( rotate(p1) ) ));
+    // }
+    // dz += 1*dt;
+    angle += Math.PI / 10 * dt;
     setTimeout(animate, 1000/FPS);
 }
 
 const FPS = 60;
 const dt = 1/FPS;
-let dz = 0;
-let z = 10; // z axis positive is towards the viewer, negative inside screen.
 let angle = 0;
 const vertices = [
     {x:  0.25, y:  0.25, z:  0.25},
@@ -99,7 +139,6 @@ const edges = [
     [4, 5], [5, 6], [6, 7], [7, 4],
     [0, 4], [1, 5], [2, 6], [3, 7]
 ]
-
 clear();
 animate();
 
