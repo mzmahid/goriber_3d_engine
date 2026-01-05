b124da7de62e72f6d891661b708d920b0b4f75c0
MZMahid <78899301+MZMahid@users.noreply.github.com>
2026-01-05 23:52:28 +0600

    include manually parsed vertex and faces

--- cube.js
+++ cube.js
@@ -14,18 +14,18 @@ const fs = [
     // Front face (vertices 0, 1, 2, 3)
     [0, 1, 2], [0, 2, 3], // Front (2 triangles)
 
-    // Back face (vertices 4, 5, 6, 7)
+    // // Back face (vertices 4, 5, 6, 7)
     [4, 5, 6], [4, 6, 7], // Back (2 triangles)
 
-    // Top face (vertices 0, 1, 5, 4)
+    // // Top face (vertices 0, 1, 5, 4)
     [0, 1, 5], [0, 5, 4], // Top (2 triangles)
 
-    // Bottom face (vertices 3, 2, 6, 7)
+    // // Bottom face (vertices 3, 2, 6, 7)
     [3, 2, 6], [3, 6, 7], // Bottom (2 triangles)
 
-    // Left face (vertices 1, 2, 6, 5)
+    // // Left face (vertices 1, 2, 6, 5)
     [1, 2, 6], [1, 6, 5], // Left (2 triangles)
 
-    // Right face (vertices 0, 3, 7, 4)
+    // // Right face (vertices 0, 3, 7, 4)
     [0, 3, 7], [0, 7, 4], // Right (2 triangles)
 ];
